794433c9eb88b211ec1d6235a7d8a53727128a44
Aubert Nicolas <nicolas.aubert1@he-arc.ch>
2022-03-16 13:48:19 +0100

    Changing base project for CMake

--- ArcStreamLabs/mainwindow.h
+++ ArcStreamLabs/mainwindow.h
@@ -9,15 +9,16 @@ QT_BEGIN_NAMESPACE
     namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
+    class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-public:
-    MainWindow(QWidget *parent = nullptr);
+        public:
+                 MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+
 };
 #endif // MAINWINDOW_H
