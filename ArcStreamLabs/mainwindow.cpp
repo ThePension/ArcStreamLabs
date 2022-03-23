712117532a8652ca0c71ff6c847819c2ca554905
Vuilliomenet Théo <theo.vuilliom@he-arc.ch>
2022-03-23 15:52:40 +0100

    Create second version of the UI

--- ArcStreamLabs/mainwindow.cpp
+++ ArcStreamLabs/mainwindow.cpp
@@ -1,15 +1,50 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    configureUI();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::configureUI()
+{
+    QPixmap play = QPixmap(":/Images/Play.png");
+    play = play.scaled(50, 50, Qt::KeepAspectRatio);
+    ui->btnPlay->setIcon(play);
+    ui->btnPlay->setFlat(true);
+    ui->btnPlay->setFocusPolicy(Qt::NoFocus);
+    ui->btnPlay->setIconSize(play.size());
+    ui->btnPlay->setStyleSheet("QPushButton:flat:pressed { border: none; };");
+
+    QPixmap pause = QPixmap(":/Images/Pause.png");
+    pause = pause.scaled(50, 50, Qt::KeepAspectRatio);
+    ui->btnPause->setIcon(pause);
+    ui->btnPause->setFlat(true);
+    ui->btnPause->setFocusPolicy(Qt::NoFocus);
+    ui->btnPause->setIconSize(pause.size());
+    ui->btnPause->setStyleSheet("QPushButton:flat:pressed { border: none; };");
+
+    QPixmap stop = QPixmap(":/Images/Stop.png");
+    stop = stop.scaled(50, 50, Qt::KeepAspectRatio);
+    ui->btnStop->setIcon(stop);
+    ui->btnStop->setFlat(true);
+    ui->btnStop->setFocusPolicy(Qt::NoFocus);
+    ui->btnStop->setIconSize(stop.size());
+    ui->btnStop->setStyleSheet("QPushButton:flat:pressed { border: none; };");
+
+    QPixmap snapshot = QPixmap(":/Images/Snapshot.png");
+    snapshot = snapshot.scaled(80, 80, Qt::KeepAspectRatio);
+    ui->btnInstantane->setIcon(snapshot);
+    ui->btnInstantane->setFlat(true);
+    ui->btnInstantane->setFocusPolicy(Qt::NoFocus);
+    ui->btnInstantane->setIconSize(snapshot.size());
+    ui->btnInstantane->setStyleSheet("QPushButton:flat:pressed { border: none; };");
 }
 
--- ArcStreamLabs/mainwindow.h
+++ ArcStreamLabs/mainwindow.h
@@ -1,24 +1,25 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#pragma once
 
 #include <QMainWindow>
-
 #include <opencv2/opencv.hpp>
+#include <QMutex>
 
 QT_BEGIN_NAMESPACE
     namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-    class MainWindow : public QMainWindow
+class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
-        public:
-                 MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    public:
+        MainWindow(QWidget *parent = nullptr);
+        ~MainWindow();
 
-private:
-    Ui::MainWindow *ui;
-    cv::Mat mat;
+    private:
+        QMutex mutex;
+        Ui::MainWindow *ui;
+        cv::Mat mat;
+
+        void configureUI();
 };
-#endif // MAINWINDOW_H
