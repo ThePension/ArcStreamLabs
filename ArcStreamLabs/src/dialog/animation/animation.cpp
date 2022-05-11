0f98e9904ca60a86d0f256fb5ed107a9607c5f5c
nicolas.aubert1 <nicolas.aubert1@hes-so.ch>
2022-05-11 14:58:02 +0200

    Adding GUI for animations

--- ArcStreamLabs/src/dialog/animation/animation.cpp
+++ ArcStreamLabs/src/dialog/animation/animation.cpp
@@ -1,8 +1,56 @@
 #include "animation.h"
 
+#include "../../arcstreamlab.h"
+
+#include <QPushButton>
+
 Animation::Animation(QWidget *parent) : QDialog(parent)
 {
     setWindowTitle("Animation");
     resize(200, 100);
+
+    geometry();
+    control();
+    appearance();
+
+    this->parent = static_cast<ArcStreamLab*>(parent);
+
+    actUndo = new QAction("undo", this);
+    actUndo->setShortcut(QKeySequence("Ctrl+Z"));
+    connect(actUndo, &QAction::triggered, this->parent, &ArcStreamLab::undo);
+
+    actRedo = new QAction("redo", this);
+    actRedo->setShortcut(QKeySequence("Ctrl+Y"));
+    connect(actRedo, &QAction::triggered, this->parent, &ArcStreamLab::redo);
+
+    addAction(actUndo);
+    addAction(actRedo);
+}
+
+void Animation::sloButtonMustacheAnimation()
+{
+    AnimationActions * mustacheAnimation = new MustacheAnimation();
+    emit sigSetMustacheAnimation(mustacheAnimation);
+}
+
+
+void Animation::geometry()
+{
+    this->buttonMustacheAnimation = new QPushButton(this);
+    this->buttonMustacheAnimation->setText(tr("Mustache animation"));
+
+    this->verticalBox = new QVBoxLayout();
+    this->verticalBox->addWidget(this->buttonMustacheAnimation);
+
+    setLayout(this->verticalBox);
+}
+
+void Animation::control()
+{
+    connect(this->buttonMustacheAnimation, &QPushButton::clicked, this, &Animation::sloButtonMustacheAnimation);
+}
+
+void Animation::appearance()
+{
     this->setStyleSheet("background : url(:/img/back6.png)");
 }
--- ArcStreamLabs/src/dialog/animation/animation.h
+++ ArcStreamLabs/src/dialog/animation/animation.h
@@ -2,24 +2,37 @@
 
 #include <QDialog>
 
+#include "mustacheanimation.h"
+
 class QCheckBox;
 class QLabel;
 class QLineEdit;
 class QPushButton;
+class QVBoxLayout;
+class ArcStreamLab;
 
 class Animation : public QDialog
 {
     Q_OBJECT
 
     private:
-        // TODO
+        QVBoxLayout *verticalBox;
+
+        QPushButton * buttonMustacheAnimation;
+
+        ArcStreamLab *parent;
+        QAction *actUndo, *actRedo;
+
+        void geometry();
+        void control();
+        void appearance();
 
     public:
         Animation(QWidget *parent = 0);
 
     signals:
-        // TODO
+        void sigSetMustacheAnimation(AnimationActions *);
 
     private slots:
-        // TODO
+        void sloButtonMustacheAnimation();
 };
